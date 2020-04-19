a4b0bfc16060b0af2dca686d0e10da9c006b5011
Luiz Casula <luizcasulaf@gmail.com>
2020-04-19 14:22:45 -0400

    Update get file ics

--- bot_ava.py
+++ bot_ava.py
@@ -5,8 +5,11 @@ from icalendar import Calendar, Event
 from datetime import datetime
 from pytz import UTC
 import os
+import time
+import secret
 
-DOWNLOAD_PATH = r''
+
+DATA_PATH = r'C:\Users\lacft\Documents\Python Projects\AVA bot\moddle_calendar_bot\data'
 
 class Bot:
     
@@ -14,15 +17,15 @@ class Bot:
         self.username = username
         self.password = password
 
-    def getFileName(self):
-        path = DOWNLOAD_PATH
+    def getFilesInPath(self):
+        path = DATA_PATH
         for p, _, files in os.walk(os.path.abspath(path)):
             for file in files:
                 print(os.path.join(p, file))
 
     def getFile(self):
         options = webdriver.ChromeOptions()
-        preferences = {'download.default_directory': '{}'.format(DOWNLOAD_PATH), 'safebrowsing.enable': 'false'}
+        preferences = {'download.default_directory': '{}'.format(DATA_PATH), 'safebrowsing.enable': 'false'}
         options.add_experimental_option("prefs", preferences)
         driver = webdriver.Chrome(chrome_options=options)
         driver.get('https://virtual.ifro.edu.br/jiparana/calendar/export.php')
@@ -39,7 +42,8 @@ class Bot:
         
 
     def readIcsFile(self):
-        file = open('icalexport.ics','rb')
+            
+        file = open('data/icalexport.ics','rb')
         calendar = Calendar.from_ical(file.read())
         for component in calendar.walk():
             if component.name == "VEVENT":
@@ -47,10 +51,38 @@ class Bot:
                 print(component.get('categories'))
                 print(component.get('description'))      
         file.close()
+
+    def getLink(self):
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome()
+        driver.get('https://virtual.ifro.edu.br/jiparana/calendar/export.php')
+        sleep(2)
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys(self.username)
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys(self.password)
+        sleep(2)
+        driver.find_element_by_xpath('//*[@id="loginbtn"]').click()
+        sleep(2)
+        driver.find_element_by_xpath('//*[@id="id_events_exportevents_all"]').click()
+        driver.find_element_by_xpath('//*[@id="id_period_timeperiod_recentupcoming"]').click()
+        sleep(2)
+        driver.find_element_by_xpath('//*[@id="id_generateurl"]').click()
+        sleep(2)
+        link = driver.find_element_by_xpath('//*[@id="region-main"]/div/div/div').text
+        return link
+
+    def buildFileName(self):
+        local_time = time.localtime()
+        file_name = "{0}-{1}-{2}-{3}-{4}".format(local_time.tm_year, local_time.tm_mon, local_time.tm_mday, local_time.tm_hour, local_time.tm_sec)
+        print(file_name)
+        
             
-bot = Bot('', '')
+bot = Bot(secret.USERNAME, secret.PASSWORD)
 #bot.getFile()
 #bot.readIcsFile()
-bot.getFileName()
+#bot.getFileName()
+
+
+
+
 
 
