a11b80caf8f18f9c9c8acc5df69f60cbee3e15e9
Luiz Casula <luizcasulaf@gmail.com>
2020-04-27 23:50:35 -0400

    update methods

--- bot_ava.py
+++ bot_ava.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from time import sleep
-
 from icalendar import Calendar, Event
 from datetime import datetime
 from pytz import UTC
@@ -15,70 +14,78 @@ DATA_PATH = r'C:\Users\lacft\Documents\Python Projects\AVA bot\moddle_calendar_b
 
 
 class Bot:
-    
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        
-
-    def getFilesInPath(self):
-        path = DATA_PATH
-        for p, _, files in os.walk(os.path.abspath(path)):
-            for file in files:
-                if len(files) == 1:
-                    return os.path.join(file)
-                else:
-                    #delete older file
-                    #return new file
-                    print(os.path.join(file))
 
     def getFile(self):
-        options = webdriver.ChromeOptions()
-        preferences = {'download.default_directory': '{}'.format(DATA_PATH), 'safebrowsing.enable': 'false'}
-        options.add_experimental_option("prefs", preferences)
-        driver = webdriver.Chrome(chrome_options=options)
-        driver.get('https://virtual.ifro.edu.br/jiparana/calendar/export.php')
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="username"]').send_keys(self.username)
-        driver.find_element_by_xpath('//*[@id="password"]').send_keys(self.password)
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="loginbtn"]').click()
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="id_events_exportevents_all"]').click()
-        driver.find_element_by_xpath('//*[@id="id_period_timeperiod_recentupcoming"]').click()
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="id_export"]').click()
-        sleep(5)
-        driver.quit()
+        file_in_path = self.getFileInPath()
+        if(file_in_path  == False):
+            self.downloadFile()
+            file_name = self.renameFile()
+            self.readIcsFile('{}.ics'.format(file_name))
+            
+        elif(self.calculateDeltaTime(file_in_path)):
+            self.readIcsFile(file_in_path)
+        else:
+            self.deleteFile(file_in_path)
+            self.getFile()
 
+    def downloadFile(self):
+        try:
+            options = webdriver.ChromeOptions()
+            preferences = {'download.default_directory': '{}'.format(DATA_PATH), 'safebrowsing.enable': 'false'}
+            options.add_experimental_option("prefs", preferences)
+            driver = webdriver.Chrome(chrome_options=options)
+            driver.get('https://virtual.ifro.edu.br/jiparana/calendar/export.php')
+            sleep(2)
+            driver.find_element_by_xpath('//*[@id="username"]').send_keys(self.username)
+            driver.find_element_by_xpath('//*[@id="password"]').send_keys(self.password)
+            sleep(2)
+            driver.find_element_by_xpath('//*[@id="loginbtn"]').click()
+            sleep(2)
+            driver.find_element_by_xpath('//*[@id="id_events_exportevents_all"]').click()
+            driver.find_element_by_xpath('//*[@id="id_period_timeperiod_recentupcoming"]').click()
+            sleep(2)
+            driver.find_element_by_xpath('//*[@id="id_export"]').click()
+            sleep(5)
+            driver.quit()
+            print('download file sucessful')
+        except:
+            print('download file error')
+        
+    def getFileInPath(self):
+        path = DATA_PATH
+        for p, _, files in os.walk(os.path.abspath(path)):
+            if (len(files) == 0):
+                return False
+            else:
+                for file in files:
+                    print(os.path.join(file))
+                    if(len(files) == 1):
+                        return os.path.join(file)
+                    #else:
+                        #delete older file
+                        #return newest file
+                    
+    
     def readIcsFile(self, file_name):
-        file = open('data/{}.ics'.format(file_name),'rb')
-        calendar = Calendar.from_ical(file.read())
-        for component in calendar.walk():
-            if component.name == "VEVENT":
-                print(component.get('summary'))
-                print(component.get('categories'))
-                print(component.get('description'))      
-        file.close()
+        try:
+            file = open('data/{}'.format(file_name),'rb')
+            calendar = Calendar.from_ical(file.read())
+            print('file {} opened'.format(file_name))
+            for component in calendar.walk():
+                if component.name == "VEVENT":
+                    print(component.get('summary'))
+                    print(component.get('categories'))
+                    #print(component.get('description'))
+            file.close()
+            print('file {} closed'.format(file_name))
+        except:
+            print('read file error')
 
-    def getLink(self):
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome()
-        driver.get('https://virtual.ifro.edu.br/jiparana/calendar/export.php')
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="username"]').send_keys(self.username)
-        driver.find_element_by_xpath('//*[@id="password"]').send_keys(self.password)
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="loginbtn"]').click()
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="id_events_exportevents_all"]').click()
-        driver.find_element_by_xpath('//*[@id="id_period_timeperiod_recentupcoming"]').click()
-        sleep(2)
-        driver.find_element_by_xpath('//*[@id="id_generateurl"]').click()
-        sleep(2)
-        link = driver.find_element_by_xpath('//*[@id="region-main"]/div/div/div').text
-        driver.quit()
-        return link
+    
 
     def buildFileName(self):
         if platform.system == 'Windows':
@@ -89,33 +96,40 @@ class Bot:
             return stat.st_birthtime
         except AttributeError:
             return stat.st_mtime
-                  
+
     def renameFile(self):
-        file_name = self.buildFileName()
-        sleep(3)
-        os.rename(r'{}\icalexport.ics'.format(DATA_PATH), r'{}\{}.ics'.format(DATA_PATH, file_name))
-        return file_name
-        
-    def deleteFile(self):
-        file_name = '1587885349.6933017.ics'
-        os.remove(r'{}\{}'.format(DATA_PATH, file_name))
+        try:
+            file_name = self.buildFileName()
+            os.rename(r'{}\icalexport.ics'.format(DATA_PATH), r'{}\{}.ics'.format(DATA_PATH, file_name))
+            print('rename file sucessful')
+            return file_name
+        except:
+            print('rename file error')
 
-    def convertTime(self, name):
-        #name = self.getFileName()
+    def deleteFile(self, file_name):
+        try:
+            os.remove(r'{}\{}'.format(DATA_PATH, file_name))
+            print('delete file sucessful')
+        except:
+            print('delete file error')
+
+
+    def calculateDeltaTime(self, name):
         print(name)
-        #print(datetime.fromtimestamp(name).strftime('%Y-%m-%d %H:%M:%S'))
-        print(datetime.fromtimestamp(name))
+        #tratar string
+        name = name.split('.ics')
+        print(name[0])
+        date_file = datetime.fromtimestamp(float(name[0]))
+        now = datetime.now()
+        delta = now - date_file
+        print(delta)
+        print(delta.total_seconds())
 
-    #def calculateTime(self):
-        
+        if(delta.total_seconds() < 20):
+            return True
+        else:
+            return False
 
 
 bot = Bot(secret.USERNAME, secret.PASSWORD)
-
-#bot.getFile()
-#name = bot.getFilesInPath()
-#file_name = bot.renameFile()
-#bot.getFilesInPath()
-#bot.readIcsFile(file_name)
-
-bot.convertTime(1587941903.5602446)
+bot.getFile()       
